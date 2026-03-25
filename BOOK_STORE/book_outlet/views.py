0f721c1ae25aabf0abd2757e8e025d5bb259c85c
jess <jchurgy@gmail.com>
2026-03-25 09:59:12 -0400

    give slug db index

--- BOOK_STORE/book_outlet/views.py
+++ BOOK_STORE/book_outlet/views.py
@@ -8,8 +8,11 @@ def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {'books': books})
 
-def book_detail(request, id):
-    book = get_object_or_404(Book, pk=id)
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html', {
-        "book": book
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestseller
         })
