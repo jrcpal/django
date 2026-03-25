11792e79e7b35280e5de8892bce614669e9500cb
jess <jchurgy@gmail.com>
2026-03-25 09:16:38 -0400

    add model urls, add slugs, overwrite save

--- BOOK_STORE/book_outlet/models.py
+++ BOOK_STORE/book_outlet/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestseller = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) 
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
